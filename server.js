c7a3ff3f50451dde637ba5610adbdde4e1f98330
Chinna0107 <kancharlahemanth89@gmail.com>
2025-12-15 17:28:39 +0530

    Contact Updated

--- server.js
+++ server.js
@@ -14,6 +14,20 @@ app.get('/', (req, res) => {
   res.json({ message: 'Portfolio Backend API is running!' });
 });
 
+const allowedOrigins = [
+  'https://hemanthkancharla.vercel.app',
+  'https://hemanthkancharla-git-main-chinna0107s-projects.vercel.app',
+  'http://localhost:3000',
+  'http://localhost:5000'
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Routes
 const contactRoutes = require('./routes/contact');
 app.use('/api/contact', contactRoutes);
